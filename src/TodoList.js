dfb63955411994d0c2eb2d2a523952c7b98048e8
Coskun <copolat@yahoo.comcopolat git config --global user.name copolat>
2021-01-23 15:39:27 -0600

    Create three components

---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>This is To-Do List component</h1>
+      </div>
+    )
+  }
+}
+
+export default TodoList;
