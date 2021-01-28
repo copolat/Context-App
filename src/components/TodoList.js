0174ddc047c4a08655faa24a3ca53231ba1ac5ea
Coskun <copolat@yahoo.comcopolat git config --global user.name copolat>
2021-01-27 21:42:22 -0600

    Clean up some codes

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,36 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {ThemeContext} from '../contexts/ThemeContext';
 import {TodoItemsContext} from '../contexts/TodoItemsContext';
 
 function TodoList() {
-  const {items} = useContext(TodoItemsContext)
-    const {isLightTheme, lightTheme, darkTheme} = useContext(ThemeContext);;
-    const theme = isLightTheme ? lightTheme : darkTheme
+  const [title, setTitle] = useState('');
+  const {items, addItem, removeItem} = useContext(TodoItemsContext)
+  const {isLightTheme, lightTheme, darkTheme} = useContext(ThemeContext);;
+  const theme = isLightTheme ? lightTheme : darkTheme;
+
+  const handleInput = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addItem(title);
+    setTitle('')
+  }
+
+  const handleClick = (e) => {
+    removeItem(e.target.id)
+  }
+
     return (
       <div className='todolist' style={{backgroundColor:theme.body, color: theme.text}}>
-        <input type="text"/>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={handleInput} value={title}/>
+        </form>
         <ul>
           {(items.map(items =>{
-            return <li key={items.id} className='far fa-trash-alt'>{items.title}</li>
+            return <li id={items.id} key={items.id} className='far fa-trash-alt' onClick={()=>{removeItem(items.id)}}>{items.title}</li>
           }))}
-          
-          <li className='far fa-trash-alt'>Get some milk</li>
-          <li className='far fa-trash-alt'>Pay the bills</li>
         </ul>
       </div>
     )
--- src/contexts/TodoItemsContext.js
+++ src/contexts/TodoItemsContext.js
@@ -4,13 +4,19 @@ export const TodoItemsContext = createContext();
 
 
 function TodoItemsContextProvider(props) {
-  const [items, setItems] = useState([
-    {title: "Go to the market", id: 1},
-    {title: "Get some milk", id: 2},
-    {title: "Pay the bills", id: 3}
-  ]);
+  const [items, setItems] = useState([]);
+
+  const addItem = (title) => {
+    setItems([...items, {title: title, id: Date.now()}])
+   
+  }
+
+  const removeItem = (id) => {
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray)
+  }
   return (
-    <TodoItemsContext.Provider value={{items}}>
+    <TodoItemsContext.Provider value={{items, addItem: addItem, removeItem: removeItem}}>
       {props.children}
     </TodoItemsContext.Provider>
   )
